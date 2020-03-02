8408b4da68d4869a732d614e8f3dbca81ec7f140
asbillings07 <superswiftdeveloper@gmail.com>
2020-03-02 04:45:56 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { useStore } from './Store'
-function App() {
+function App () {
   const store = useStore()
   return (
     <div className='App'>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
+import { Provider } from './Store'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
+  const { getByText } = render(
+    <Provider>
+      <App />
+    </Provider>
+  )
+  const linkElement = getByText(/Aaron Billings/i)
   expect(linkElement).toBeInTheDocument()
 })
