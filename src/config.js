ae8ff89d301654ddf2c91fa4e3adcfd153c25174
asbillings07 <superswiftdeveloper@gmail.com>
2020-03-07 23:36:28 -0500

    update url

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const apiUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=Atlanta&mode=json&cnt=5&units=imperial&APPID=d8c84f0e565326a65feb0491a79ee482'
+export const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily?q=Atlanta&mode=json&cnt=5&units=imperial&APPID=d8c84f0e565326a65feb0491a79ee482'
